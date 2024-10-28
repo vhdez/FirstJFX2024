4c6726d411ef33a9cae6cc98144dd39a955c9576
vhdez <vhernandez@scienceleadership.org>
2024-10-28 09:37:43 -0400

    Add cookie image to button.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import javafx.application.Application;
-import javafx.event.Event;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import java.io.FileInputStream;
 
 
 public class Main extends Application {
@@ -18,26 +19,36 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println("THEN");
+
         // Controls
         Label label1 = new Label("Welcome to your Fave App");
         label1.setLineSpacing(10);
         label1.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+
         Text text1 = new Text("# of Cookies: "+ numOfCookies);
-        text1.setFont(Font.font("Arial", FontWeight.BOLD, 36));
-        Button button1 = new Button("Click Me");
+        text1.setFont(Font.font("Arial", FontWeight.BOLD, 36));;
+
+        FileInputStream input = new FileInputStream("src/cookie.png");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(250);
+        imageView.setFitWidth(250);
+        Button button1 = new Button("Click Me", imageView);
+        //Button button1 = new Button("Click Me");
         button1.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+        button1.setMinSize(200,200);
         button1.setOnAction(actionEvent -> {
             System.out.println("NOW");
             numOfCookies = numOfCookies + 1;
             text1.setText("# of Cookies: "+ numOfCookies);
         });
 
-        // Layout
+        // Layouts
         HBox hbox1 = new HBox(button1, text1);
         VBox vbox2 = new VBox(label1, hbox1);
 
         // Scenes
-        Scene scene1 = new Scene(vbox2,600,200);
+        Scene scene1 = new Scene(vbox2,800,300);
 
         // Stages
         primaryStage.setScene(scene1);
